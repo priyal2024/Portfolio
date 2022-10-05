79221115280fe1f0d4feedbec3581b665a404b26
PriyalCitrusbug <priyal.citrusbug@gmail.com>
2022-10-05 22:59:30 +0100

    build portfoilio using react,nextjs and tailwind css

---
+++ pages/data.js
@@ -0,0 +1,35 @@
+import web1 from "../public/assets/messaging.avif";
+import web2 from "../public/assets/travel-advisor-web.jpg";
+import web3 from "../public/assets/movies.jpg";
+import web4 from "../public/assets/food.avif";
+
+export const data = [
+    {
+        id: 1,
+        name: "Chat Room",
+        desc: "This project is developed with ReactJs, Html, CSS,JavaScript and NodeJs. Where I have used socket.io for client - server communication. In this chatroom, users can create a room and invite friends to join. Multiple users can chat with emoji functionality. The app is responsive for different devices with a user- friendly UI.",
+        link: "https://5ee47183fb97c1729f1193bc--kind-almeida-fe1022.netlify.app",
+        img: web1
+    },
+    {
+        id: 2,
+        name: "Travel Advisor",
+        desc: "Travel Advisor is a web-application build using ReactJs, Material-UI components, Geolocation, Google Maps API & Google Places API, fetching API data based on the location which will ask user for their current location and according to that it will give list of Hotels, Restaurants and Attractions. It also gives features like filtering.",
+        link: "https://traveladvisor-reactjs-app.netlify.app",
+        img: web2
+    },
+    {
+        id: 3,
+        name: "Movie List",
+        desc: "Movie list reactjs web app which lists nowp laying movies, upcoming and latest movies. It is also provide searching movies functionality.",
+        link: "",
+        img: web3
+    },
+    {
+        id: 4,
+        name: "Food Ordering App",
+        desc: "Food list will display with it's amount,description. here user can add items into the cart and can view order details. This web app is build by using ReactJs framework and Firebase. This is build with advance react concepts like context,redux etc.",
+        link: "https://food-order-reactjs-app.netlify.app/",
+        img: web4
+    }
+]
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin')
+
+const MyClass = plugin(function ({ addUtilities }) {
+  addUtilities({
+    ".my-rotate-y-180": {
+      transform: "rotateY(180deg)"
+    },
+    ".preserve-3d": {
+      transformStyle: "preserve-3d",
+    },
+    ".perspective": {
+      perspective: "1000px"
+    },
+    ".backface-hidden": {
+      backfaceVisibility: "hidden",
+    }
+  })
+})
 module.exports = {
   darkMode: "class",
   content: [
@@ -13,5 +31,5 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [MyClass],
 };
